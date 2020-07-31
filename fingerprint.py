dc6e0574d37d38ad22ce1ee78fee7a7b852e491f
Mauro Jurada <37582356+MauroJurada@users.noreply.github.com>
2020-08-01 01:27:26 +0200

    all the fingerprints

--- fingerprint.py
+++ fingerprint.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy import interpolate
 
-with open('Mauro.csv', newline='', encoding="utf8") as csvfile:
+with open('Paper-features-formated.csv', newline='', encoding="utf8") as csvfile:
     data = list(csv.reader(csvfile))
 
 number_of_collumns = len(data[0])
@@ -39,6 +39,7 @@ for j in range(3, number_of_collumns):
     temp = []
     sum = 0
     for i in range(1, number_of_rows):
+        print("aa" + data[i][j])
         temp.append(float(data[i][j]))
     temp = numpy.array(temp)
     normalised_data_mean.append(temp.mean(dtype=numpy.float64))
@@ -46,97 +47,113 @@ for j in range(3, number_of_collumns):
     normalised_data_max.append(temp.max())
     normalised_data_std.append(temp.std(dtype=numpy.float64))
 
-j = 3
-for i in range(3, number_of_collumns):
-    temp = float(data[j][i]) - normalised_data_mean[i]
-    if temp > 0:
-        temp = (temp / (normalised_data_max[i] - normalised_data_mean[i])) * 5  # times 5 since teh dot will be +-5
-    else:
-        temp = (temp / (normalised_data_mean[i] - normalised_data_min[i])) * 5
-    finalData.append(temp)
+for j in range(1, number_of_rows):
+    finalData = []
+    for i in range(3, number_of_collumns):
+        temp = float(data[j][i]) - normalised_data_mean[i]
+        if temp > 0:
+            if (normalised_data_max[i] - normalised_data_mean[i]) == 0:
+                temp = 0
+            else:
+                temp = (temp / (normalised_data_max[i] - normalised_data_mean[i])) * 5  # times 5 since teh dot will be +-5
+        else:
+            if (normalised_data_mean[i] - normalised_data_min[i]) == 0:
+                temp = 0
+            else:
+                temp = (temp / (normalised_data_mean[i] - normalised_data_min[i])) * 5
+        finalData.append(temp)
 
-# print("finalData:")
-# print(finalData)
-#
-# print("mean:")
-# print(normalised_data_mean)
-# print("min:")
-# print(normalised_data_min)
-# print("max")
-# print(normalised_data_max)
-# print("std:")
-# print(normalised_data_std)
+    # print("finalData:")
+    # print(finalData)
+    #
+    # print("mean:")
+    # print(normalised_data_mean)
+    # print("min:")
+    # print(normalised_data_min)
+    # print("max")
+    # print(normalised_data_max)
+    # print("std:")
+    # print(normalised_data_std)
 
-coords = []
-print(len(finalData) // 2)
-for i in range(len(finalData) // 2):
-    coords.append([finalData[i] - 5, i])
+    coords = []
+    #print(len(finalData) // 2)
+    for i in range(len(finalData) // 2):
+        coords.append([finalData[i] - 5, i])
 
-for i in range(len(finalData) // 2, len(finalData)):
-    coords.append([finalData[i] + 5, 11 - i])
+    for i in range(len(finalData) // 2, len(finalData)):
+        coords.append([finalData[i] + 5, 11 - i])
 
-print(coords)
-x, y = numpy.split(coords, [-1], axis=1)
-x = numpy.ravel(x)
-y = numpy.ravel(y)
-print(x)
-# plt.scatter(x,y)
+    f = open('fingerprints/' + data[j][1] + '.txt', 'w')
+    for coord in coords:
+        f.write(str(coord) + '\n')
+    f.close()
 
-plt.xlim([-11,11])
+    print(coords)
+    x, y = numpy.split(coords, [-1], axis=1)
+    x = numpy.ravel(x)
+    y = numpy.ravel(y)
+    print(x)
+    # plt.scatter(x,y)
 
-plt.plot(x, y, lw=50, color='aliceblue', solid_capstyle='round', marker="o", markersize=53)
-plt.plot(x, y, lw=20, color='#cfcfcf', solid_capstyle='round')
+    plt.xlim([-11, 11])
 
-plt.plot([-5,-5], [0, 5.4], color='cornflowerblue', linewidth=0.6)
-plt.plot([5,5], [0, 5.4], color='cornflowerblue', linewidth=0.6)
+    plt.plot(x, y, lw=50, color='aliceblue', solid_capstyle='round', marker="o", markersize=53)
+    plt.plot(x, y, lw=20, color='#cfcfcf', solid_capstyle='round')
 
-plt.plot(x, y, lw=10, color='tab:grey', solid_capstyle='round')
-x *= 0.95
-y *= 0.99
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 0.95
-y *= 0.99
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 0.95
-y *= 0.99
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 0.95
-y *= 0.99
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 0.88
-y *= 0.99
-plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 0.88
-y *= 0.99
-plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    plt.plot([-5, -5], [0, 5.4], color='cornflowerblue', linewidth=0.6)
+    plt.plot([5, 5], [0, 5.4], color='cornflowerblue', linewidth=0.6)
 
-x, y = numpy.split(coords, [-1], axis=1)
-x = numpy.ravel(x)
-y = numpy.ravel(y)
+    plt.plot(x, y, lw=10, color='tab:grey', solid_capstyle='round')
+    x *= 0.95
+    y *= 0.99
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 0.95
+    y *= 0.99
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 0.95
+    y *= 0.99
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 0.95
+    y *= 0.99
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 0.88
+    y *= 0.99
+    plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 0.88
+    y *= 0.99
+    plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
 
-x *= 1.05
-y *= 1.01
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 1.05
-y *= 1.01
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 1.05
-y *= 1.01
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 1.05
-y *= 1.01
-plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 1.07
-y *= 1.01
-plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
-x *= 1.07
-y *= 1.01
-plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x, y = numpy.split(coords, [-1], axis=1)
+    x = numpy.ravel(x)
+    y = numpy.ravel(y)
 
-x, y = numpy.split(coords, [-1], axis=1)
-x = numpy.ravel(x)
-y = numpy.ravel(y)
-plt.plot(x, y, color='k', marker=',', linestyle='solid', linewidth=2, solid_capstyle='round')
-plt.text(10, -0.2, 'v0.1', fontsize=6)
-plt.axis('off')
-plt.show()
+    x *= 1.05
+    y *= 1.01
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 1.05
+    y *= 1.01
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 1.05
+    y *= 1.01
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 1.05
+    y *= 1.01
+    plt.plot(x, y, color='#4f4f4f', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 1.07
+    y *= 1.01
+    plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+    x *= 1.07
+    y *= 1.01
+    plt.plot(x, y, color='gainsboro', marker='', linestyle='solid', linewidth=1, solid_capstyle='round')
+
+    x, y = numpy.split(coords, [-1], axis=1)
+    x = numpy.ravel(x)
+    y = numpy.ravel(y)
+    plt.plot(x, y, color='k', marker=',', linestyle='solid', linewidth=2, solid_capstyle='round')
+    plt.text(10, -0.2, 'v0.1', fontsize=6)
+    plt.axis('off')
+    plt.savefig('fingerprints/' + data[j][1] + '.png', dpi=None, facecolor='w', edgecolor='w',
+            orientation='portrait', papertype=None, format=None,
+            transparent=False, bbox_inches='tight', pad_inches=0.1,
+            metadata=None)
+    plt.show()
